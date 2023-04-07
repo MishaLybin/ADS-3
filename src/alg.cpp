43a8431b655de300619a9f205fdd7b33c1be9e72
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-07 18:12:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count=0;
-  for(int i=0; i<size;i++){
-    if(arr[i]==value)
-      count+=1;
+  int count = 0;
+  for (int i = 0; i < size;i++) {
+    if (arr[i] == value)
+      count += 1;
   }
   return count;
 }
